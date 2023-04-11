162780ad5f1e5ba509fe75ee5f79535f231b9d6e
Sharun <kakkadsasikumar.s@northeastern.edu>
2023-04-11 14:13:09 -0400

    updated the input location

--- TravellingSalesman/src/main/java/edu/neu/christofides/Constants.java
+++ TravellingSalesman/src/main/java/edu/neu/christofides/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public static int INT_MAX_VALUE = Integer.MAX_VALUE;
     public static double DOUBLE_MAX_VALUE = Double.MAX_VALUE;
 
-    public static String DATA_SET_LOCATION = "/Users/rohitpanicker/Desktop/PSA-Final-Project/TravellingSalesman/src/main/java/edu/neu/testData/input1.txt";
+    public static String DATA_SET_LOCATION = "src/main/java/edu/neu/testData/input1.txt";
 }
