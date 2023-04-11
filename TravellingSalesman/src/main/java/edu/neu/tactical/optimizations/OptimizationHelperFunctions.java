c7a9576056c55aa9ba343e54ab1b982da81e86e6
rohitpanicker06@gmail.com <rohitpanicker06@gmail.com>
2023-04-11 00:39:32 -0400

    Making function static

--- TravellingSalesman/src/main/java/edu/neu/tactical/optimizations/OptimizationHelperFunctions.java
+++ TravellingSalesman/src/main/java/edu/neu/tactical/optimizations/OptimizationHelperFunctions.java
@@ -11,7 +11,7 @@ public class OptimizationHelperFunctions {
         return sum;
     }
 
-     void swap(int[] route, int i, int j) {
+     static void swap(int[] route, int i, int j) {
         int temp = route[i];
         route[i] = route[j];
         route[j] = temp;
