a4c60eeb329c9aab1fcf150f253b16c6b5f676a2
rohitpanicker06@gmail.com <rohitpanicker06@gmail.com>
2023-04-11 00:25:25 -0400

    Added implementation for Euler Circuit

--- TravellingSalesman/src/main/java/edu/neu/mainRunner/Main.java
+++ TravellingSalesman/src/main/java/edu/neu/mainRunner/Main.java
@@ -1,9 +1,12 @@
 package edu.neu.mainRunner;
 
+import edu.neu.christofides.EulerCircuitGenerator;
 import edu.neu.christofides.GreedyMatch;
 import edu.neu.christofides.MultiGraph;
 import edu.neu.christofides.PrimsAlgorithm;
 import edu.neu.graphs.node.GraphNode;
+
+import java.util.Arrays;
 
 public class Main {
 
@@ -15,6 +18,17 @@ public class Main {
         int[][] matchGraph = GreedyMatch.greadyMatch(minimumSpanningTree, weightMatrix, weightMatrix.length);
 
         GraphNode nodes[] = MultiGraph.build(matchGraph, minimumSpanningTree);
+        int route[] = EulerCircuitGenerator.generateEulerCircuit(nodes);
+
+        double sum=0;
+
+        for(int i=1;i<route.length;i++){
+            sum+=weightMatrix[route[i-1]][route[i]];
+        }
+        sum+=weightMatrix[route[0]][route[route.length-1]];
+        System.out.println("Route = " + Arrays.toString(route));
+        System.out.println("Total Sum : "+sum);
+
 
     }
 }
