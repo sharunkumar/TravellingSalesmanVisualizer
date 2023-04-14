ba07f576c83e1e44f3e3d7de0bd7b186203390a6
sstej88 <saitejsunkara9@gmail.com>
2023-04-13 21:48:01 -0400

    Recursion technique Implemented for Traveling Salesman Problem.

--- V101/src/main/java/edu/neu/coe/App.java
+++ V101/src/main/java/edu/neu/coe/App.java
@@ -6,8 +6,19 @@ package edu.neu.coe;
  */
 public class App 
 {
+    private static int[][] routeMap = new int[4][4];
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<4; j++) {
+                if(i==j) routeMap[i][j] = 0;
+                else routeMap[i][j] = 2;
+            }
+        }
+        routeMap[3][0] = 1;
+        routeMap[0][3] = 1;
+        RecursionTSP recursionTSP = new RecursionTSP(routeMap);
+        long travelingSalesman = recursionTSP.travelingSalesmanRecursion(1, 0);
+        System.out.println(travelingSalesman);
     }
 }
