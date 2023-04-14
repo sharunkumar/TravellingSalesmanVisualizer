0e7e34135121db4855cf1b974c03999f2324f99f
sstej88 <saitejsunkara9@gmail.com>
2023-04-14 02:12:16 -0400

    Dynamic Programming Implemented

--- V101/src/main/java/edu/neu/coe/App.java
+++ V101/src/main/java/edu/neu/coe/App.java
@@ -5,19 +5,35 @@ import java.math.*;
 import java.lang.*;
 public class App 
 {
-    private static int[][] routeMap = new int[4][4];
+    private static int[][] routeMap;
     public static void main( String[] args )
     {
-        for(int i=0; i<4; i++) {
-            for(int j=0; j<4; j++) {
-                if(i==j) routeMap[i][j] = 0;
-                else routeMap[i][j] = 2;
-            }
-        }
-        routeMap[3][0] = 1;
-        routeMap[0][3] = 1;
+//        routeMap = new int[4][4];
+//        for(int i=0; i<4; i++) {
+//            for(int j=0; j<4; j++) {
+//                if(i==j) routeMap[i][j] = 0;
+//                else routeMap[i][j] = 2;
+//            }
+//        }
+//        routeMap[3][0] = 1;
+//        routeMap[0][3] = 1;
+
+//        Initializing known routeMap and weight should be 85
+        routeMap = new int[][]{
+                {0, 20, 42, 25},
+                {20, 0, 30, 34},
+                {42, 30, 0, 10},
+                {25, 34, 10, 0}
+        };
+
         RecursionTSP recursionTSP = new RecursionTSP(routeMap);
-        long travelingSalesman = recursionTSP.travelingSalesmanRecursion(1, 0);
-        System.out.println("Traveling Salesman Minimum Weight: "+travelingSalesman);
+        DynamicProgrammingTSP dynamicProgrammingTSP = new DynamicProgrammingTSP(routeMap);
+
+        long travelingSalesmanRecursion = recursionTSP.travelingSalesmanRecursion(1, 0);
+        long travelingSalesmanDP = dynamicProgrammingTSP.travelingSalesmanDP(1, 0);
+
+        System.out.println("Traveling Salesman Minimum Weight (Recursion Technique): "+travelingSalesmanRecursion);
+        System.out.println("Traveling Salesman Minimum Weight (Dynamic Programming Technique): "+travelingSalesmanDP);
+
     }
 }
---
+++ V101/src/main/java/edu/neu/coe/DynamicProgrammingTSP.java
@@ -0,0 +1,48 @@
+package edu.neu.coe;
+import java.io.* ;
+import java.math.*;
+import java.util.*;
+
+public class DynamicProgrammingTSP {
+    public static int[][] adjacencyMatrix;
+    public static int numberOfVertices = 0;
+    public static long[][] dynamicProgrammingCache;
+    public DynamicProgrammingTSP(int[][] routeMap) {
+
+        numberOfVertices = routeMap[0].length;
+        System.out.println("Length of routeMap: "+numberOfVertices);
+        adjacencyMatrix = new int[numberOfVertices][numberOfVertices];
+        adjacencyMatrix = routeMap;
+
+        // Initializing cache with default values of -1
+        double twoPowerN = Math.pow(2, numberOfVertices);
+        dynamicProgrammingCache = new long[(int)twoPowerN][numberOfVertices]; // Because we are reducing the code complexity to O(n.2^n) complexity
+        for(int i=0; i<twoPowerN; i++) {
+            for(int j=0; j<numberOfVertices; j++) {
+                dynamicProgrammingCache[i][j] = -1;
+            }
+        }
+
+    }
+
+    public long travelingSalesmanDP(int mask, int vertex) {
+        long returnValue = (long) 1e18;
+        long answer = 0;
+        if(mask==(Math.pow(2, numberOfVertices)-1)) {
+            return adjacencyMatrix[vertex][0];
+        }
+        else {
+            if(dynamicProgrammingCache[mask][vertex]!=-1) {
+                return dynamicProgrammingCache[mask][vertex];
+            }
+            for(int i=0; i<numberOfVertices; i++) {
+                if ((mask & (1 << i)) == 0) {
+                    answer = adjacencyMatrix[vertex][i] + travelingSalesmanDP(mask | (1 << i), i);
+                    returnValue = Math.min(answer, returnValue);
+                }
+            }
+        }
+        dynamicProgrammingCache[mask][vertex] = returnValue;
+        return returnValue;
+    }
+}
